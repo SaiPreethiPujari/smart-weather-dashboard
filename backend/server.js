b3d577b50184758c8bb85b53124f75fc1309579b
P. Saipreethi <pujarisaipreethi2005@gmail.com>
2026-03-10 22:04:47 +0530

    Updated weather data retrieval logic

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
+
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
--- frontend/script.js
+++ frontend/script.js
@@ -2,20 +2,19 @@ async function getWeather(){
 
 let city = document.getElementById("city").value;
 
-if(city === ""){
-    alert("Please enter a city name");
-    return;
-}
-
 try{
 
 let response = await fetch(`http://localhost:5000/weather/${city}`);
+
+if(!response.ok){
+throw new Error("API request failed");
+}
 
 let data = await response.json();
 
 document.getElementById("weatherResult").innerHTML = `
 <h2>${data.name}</h2>
-<img class="weather-icon" src="https://openweathermap.org/img/wn/01d@2x.png" alt="weather icon">
+<img src="https://openweathermap.org/img/wn/01d@2x.png">
 <p>Temperature: ${data.main.temp} °C</p>
 <p>Weather: ${data.weather[0].description}</p>
 <p>Humidity: ${data.main.humidity}%</p>
@@ -26,7 +25,7 @@ document.getElementById("weatherResult").innerHTML = `
 catch(error){
 
 document.getElementById("weatherResult").innerHTML =
-"<p>Unable to fetch weather data</p>";
+"Unable to fetch weather data";
 
 }
 
